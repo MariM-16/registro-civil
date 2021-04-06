ce8e9411b3d45f3369cd9fafc3d3256b899bac06
MariM-16 <69335919+MariM-16@users.noreply.github.com>
2021-04-06 10:04:08 -0400

    MAIN

---
+++ Desktop/POO REGISTRO CIVIL/main.py
@@ -0,0 +1,55 @@
+from person import Person
+
+people = []
+
+def print_menu_and_select():
+    print ("\nSelecciona una de las siguientes opciones ")
+    print ("1. Crear una persona ")
+    print ("2. Envejecer una persona ")
+    print ("3. Mostrar personas ")
+    print ("4. Cambiar nombre ")
+    print ("0. Salir ")
+    return int(input())
+
+def create_person():
+    name = input("Ingrese el nombre de la persona: ")
+    age = int(input("Ingrese la edad de la persona: "))
+    people.append(Person(name,age))
+
+def show_people():
+    print("\nPesonas creadas: ")
+    for (i,_) in enumerate(people):
+        print(f"{i}:{people[i].name} = {people[i].age}" ) #preguntar esto porque no entendi
+
+def person_selector():
+    return int(input("A quien quieres seleccionar? "))
+
+def grow_old_person(): #envejecer a personas pero debo primero mostrar a todas
+    show_people()
+    i = person_selector()
+    years = int(input("En cuántos años lo quieres envejecer? "))
+    people[i].age += years
+
+def change_name():
+    i = person_selector()
+    name = input("Ingresa el nuevo nombre ")
+    people[i].name = name
+
+def menu():
+    while True:
+        selection = print_menu_and_select()
+        if selection == 0:
+            break
+        if selection == 1:
+            create_person()
+        if selection == 2:
+            grow_old_person()
+        if selection == 3:
+            show_people()
+        if selection == 4:
+            change_name()
+
+
+
+if __name__ == "__main__":
+    menu()
